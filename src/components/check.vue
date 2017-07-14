f6ad1a89d40efa86f3e6e93066702ed43bfe9a78
hexianzhi <hxz707950194@gmail.com>
2017-07-15 01:02:49 +0800

    first

---
+++ src/components/check.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    查看问卷
+  </h1>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+
+
+<style scoped>
+  h1{
+    background-color: red;
+  }
+</style>
