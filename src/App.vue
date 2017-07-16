7b80de975bff7c9aa4b39859487092ff74ab0abd
hexianzhi <hxz707950194@gmail.com>
2017-07-16 21:07:30 +0800

    主页面css框架提交

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <div>
-      asdsad
-    </div>
+    <header>
+      <router-link to="/">
+        <img src="./assets/question_mark.png"/>
+        <h3>
+          问卷管理
+        </h3>
+        <h5>
+          我的问卷
+        </h5>
+      </router-link>
+    </header>
     <router-view></router-view>
   </div>
 </template>
@@ -13,13 +21,53 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 1000px;
+  width: 100%;
+  background-color: #EFEFEF;
+
 }
+
+header{
+  color: white;
+  background-color: #EE7419;
+  width: 100%;
+  padding: 10px;
+  overflow: auto;
+  text-align: center;
+
+  img{
+
+    float: left;
+    margin-left: 100px;
+  }
+  h3{
+    color: white;
+    margin-left: 60px;
+    margin-top: 5px;
+    float: left;
+    display: inline-block;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  h5{
+    color: white;
+    margin-left: 60px;
+    margin-top: 10px;
+    float: left;
+    display: inline-block;
+    font-size: 16px;
+  }
+
+}
+
+
+
 </style>
---
+++ src/components/edit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    我是编辑页面
+  </div>
+
+</template>
+
+
+<script>
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
