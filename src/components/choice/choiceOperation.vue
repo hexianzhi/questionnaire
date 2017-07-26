c4c5d7a9199f6be9396525cf6bd8eae807099823
hexianzhi <hxz707950194@gmail.com>
2017-07-26 23:57:15 +0800

    更新目录

---
+++ src/components/choice/choiceOperation.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="container">
+      <span onselectstart="return false"  class="up" v-bind:class="{active: canUp}" @click="up">
+      上移
+       </span>
+
+      <span onselectstart="return false" class="down" v-bind:class="{ active: canDown}" @click="down">
+      下移
+      </span>
+
+      <span onselectstart="return false" class="repeat" @click="repeat">
+       复用
+      </span>
+
+      <span onselectstart="return false;" class="delete" @click="deleteChoice">
+       删除
+      </span>
+
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        props: {
+          canUp: {
+            type: Boolean,
+            default: false
+          },
+          canDown: {
+            type: Boolean,
+            default: false
+          }
+        },
+        methods: {
+          up: function () {
+            this.$emit("up")
+          },
+          down: function () {
+            this.$emit("down")
+          },
+
+          deleteChoice: function () {
+            this.$emit('deleteChoice')
+          },
+          repeat: function () {
+            this.$emit('repeat')
+          }
+        }
+    }
+</script>
+
+
+<style lang="scss" scoped>
+
+  .container{
+    span{
+      font-size: 16px;
+      padding: 10px;
+      cursor: pointer;
+    }
+    .up, .down{
+      display: none;
+    }
+    .active{
+      display: inline;
+    }
+  }
+</style>
